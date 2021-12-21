83f710700bed0f55750fe39fc01398311b5e1bc1
Iurii Polnikov <yuriy.polnikov@genesys.com>
2021-12-21 11:22:30 +0300

    added test login page

---
+++ test_login_page.py
@@ -0,0 +1,11 @@
+from pages.login_page import LoginPage
+
+link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+
+def test_login_page(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+    page.should_be_login_form()
+    page.should_be_register_form()
+
